afe3e4481bfedad9439ac657f47a997e868fd769
jtlsan <jtlsan@naver.com>
2017-11-26 19:06:16 +0900

    Add files via upload

--- 1.c
+++ 1.c
@@ -5,25 +5,43 @@
 void flush(void);
 void start(void);
 void add(int *);
+
 struct d_title{
   char * title;
   struct movie * link;
   struct d_title * next;
 };
 
-struct director{
-  int serial_number;
+typedef struct director{
+  char *tag;
+  char * serial_number;
   char * d_name;
-  char s;
-  int birth;
+  char *s;
+  char * birth;
   struct d_title * director_title;
   struct director * next;
-};
+} director;
 struct m_actor{
   char * actor;
   struct actor * link;
   struct m_actor * next;
 };
+
+
+struct a_title{
+  char * title;
+  struct movie * link;
+  struct a_title * next;
+};
+typedef struct actor{
+  char *tag;
+  char *serial_number;
+  char * actor;
+  char *s;
+  char * birth;
+  struct a_title * actor_title;
+  struct actor * next;
+} actor;
 typedef struct movie{
   char * serial_number;
   char * year;
@@ -32,33 +50,26 @@ typedef struct movie{
   char * title;
   char * genre;
   char * director;
-  struct m_actor * movie_actor;
+  struct m_actor movie_actor;
   struct movie * next;
 } movie;
 
-struct a_title{
-  char * title;
-  struct movie * link;
-  struct a_title * next;
-};
-struct actor{
-  int serial_number;
-  char * actor;
-  char s;
-  int birth;
-  struct a_title * actor_title;
-  struct actor * next;
-};
-
-void input_log(movie *, FILE *);
+void input_m_log(movie *);
+void input_a_log(actor *);
+void input_d_log(director *);
+//void input_m_actor(FILE *, movie *);
 void string_input(FILE *, char **);
 
 int main(void){
   char ** a;
   int i = 0;
   movie *m_log = (movie *)malloc(sizeof(movie));
-  FILE *movie_file;
-  input_log(m_log, movie_file);
+  actor *a_log = (actor *)malloc(sizeof(actor));
+  director *d_log = (director *)malloc(sizeof(director));
+  input_m_log(m_log);
+  input_a_log(a_log);
+  input_d_log(d_log);
+
 
 
 /*  a = (char **)malloc(sizeof(char *) * 5);
@@ -79,12 +90,10 @@ int main(void){
   return 0;
 }
 
-void input_log(movie *m_log, FILE *movie_file){
-  movie_file = fopen("movie_log.txt", "r");
-  char tmp;
-  char *s_tmp;
-  int size = 0;
+void input_m_log(movie *m_log){
 
+  FILE *movie_file;
+  movie_file = fopen("movie_log.txt", "r");
 //tag입력
   string_input(movie_file, &(m_log -> tag));
 
@@ -102,38 +111,99 @@ void input_log(movie *m_log, FILE *movie_file){
 
 //year 입력
   string_input(movie_file, &(m_log -> year));
-    size = 0;
 
 //run_time 입력
-
     string_input(movie_file, &(m_log -> run_time));
 
 //actor은 어떻게 입력하징
 
+  fclose(movie_file);
   printf("%s\n", m_log -> run_time);
   printf("%s\n", m_log -> year);
   printf("%s\n", m_log -> director);
   printf("%s\n", m_log -> genre);
   printf("%s\n", m_log -> title);
   printf("%s\n", m_log -> serial_number);
-  printf("%s", m_log -> tag);
+  printf("%s\n", m_log -> tag);
+
+
+}
+/*
+void input_m_actor(FILE *movie_file, movie *m_log){
+  char tmp;
+  int size;
+  (m_log -> movie_actor) = (struct m_actor *)malloc(sizeof(struct m_actor));
+
+  fscanf(movie_file, "%c", &tmp);   //동적 메모리 할당을 위해 입력받는 string의 크기 알아보기
+  while (tmp != ','){
+    size++;
+    fscanf(movie_file, "%c", &tmp);
+  }
+
+  m_log -> movie_actor -> actor = (char *)malloc(sizeof(char) * size + 1);
+  fseek(movie_file, -(size + 1), SEEK_CUR);
+
+  fscanf(movie_file, "%c", &tmp);
+  for (int i = 0; tmp != ','; i++){
+    *(m_log -> movie_actor -> actor + i) = tmp;
+    fscanf(movie_file, "%c", &tmp);
+  }
+
+  printf("%s\n", m_log -> movie_actor -> actor);
+
+}
+*/
+void input_d_log(director *d_log){
+  FILE *d_file = fopen("director_log.txt", "r");
+
+
+  //tag입력
+  string_input(d_file, &(d_log -> tag));
+  string_input(d_file, &(d_log -> serial_number));
+  string_input(d_file, &(d_log -> d_name));
+  string_input(d_file, &(d_log -> s));
+  string_input(d_file, &(d_log -> birth));
+
+  printf("%s\n", d_log -> tag);
+  printf("%s\n", d_log -> serial_number);
+  printf("%s\n", d_log -> d_name);
+  printf("%s\n", d_log -> s);
+  printf("%s\n", d_log -> birth);
+}
+void input_a_log(actor *a_log){
+  FILE *a_file = fopen("actor_log.txt", "r");
+
+
+  //tag입력
+  string_input(a_file, &(a_log -> tag));
+  string_input(a_file, &(a_log -> serial_number));
+  string_input(a_file, &(a_log -> actor));
+  string_input(a_file, &(a_log -> s));
+  string_input(a_file, &(a_log -> birth));
+
+  printf("%s\n", a_log -> tag);
+  printf("%s\n", a_log -> serial_number);
+  printf("%s\n", a_log -> actor);
+  printf("%s\n", a_log -> s);
+  printf("%s\n", a_log -> birth);
 
 }
 
-void string_input(FILE *movie_file, char **subject){ //반복되는 문장 함수로 만들어보는중
+void string_input(FILE *movie_file, char **subject){
   char tmp;
   char *s_tmp;
   int size;
-  fscanf(movie_file, "%c", &tmp);
+
+  fscanf(movie_file, "%c", &tmp);   //동적 메모리 할당을 위해 입력받는 string의 크기 알아보기
   while (tmp != ':'){
     size++;
     fscanf(movie_file, "%c", &tmp);
   }
 
-    (*subject) = (char *)malloc(sizeof(char) * size + 1);
-    fseek(movie_file, -(size + 1), SEEK_CUR);
+    (*subject) = (char *)malloc(sizeof(char) * size + 1); //NULL까지 고려하여 동적 메모리 할당
+    fseek(movie_file, -(size + 1), SEEK_CUR); //파일 위치 지시자를 읽기 시작했던 곳으로 돌림
 
-    fscanf(movie_file, "%c", &tmp);
+    fscanf(movie_file, "%c", &tmp);         // tmp에 먼저 저장하여 :가 나오기 전까지 subject 스트링에 입력
     for (int i = 0; tmp != ':'; i++){
       *(*subject + i) = tmp;    //**(subject + i)와 *(*subject + i)의 차이점??
       fscanf(movie_file, "%c", &tmp);
